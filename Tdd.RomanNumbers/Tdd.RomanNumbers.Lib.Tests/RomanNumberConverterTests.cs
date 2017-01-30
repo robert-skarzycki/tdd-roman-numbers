ca60ddfd51fffad51be1c7709bb4ef50804ecdea
Robert Skarżycki <robert.skarzycki@gmail.com>
2017-01-30 19:35:27 +0100

    Class and files naming - adjusted

--- Tdd.RomanNumbers/Tdd.RomanNumbers.Lib.Tests/RomanNumberConverterTests.cs
+++ Tdd.RomanNumbers/Tdd.RomanNumbers.Lib.Tests/RomanNumberConverterTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Tdd.RomanNumbers.Lib.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class RomanNumberConverterTests
     {
         [TestMethod]
         public void TestMethod1()
