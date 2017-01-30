da7d592415f9ea76f1b9d61d7ebb50a1e67def31
Robert Skarżycki <robert.skarzycki@gmail.com>
2017-01-30 19:50:39 +0100

    Input validation - empty string and null check

--- Tdd.RomanNumbers/Tdd.RomanNumbers.Lib.Tests/RomanNumberConverterTests.cs
+++ Tdd.RomanNumbers/Tdd.RomanNumbers.Lib.Tests/RomanNumberConverterTests.cs
@@ -1,14 +1,26 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
+using System;
+
 
 namespace Tdd.RomanNumbers.Lib.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class RomanNumberConverterTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void WHEN_empty_string_passed_THEN_returns_zero()
         {
+            var sut = new RomanNumbersConverter();
+            var result = sut.ToInteger(string.Empty);
+
+            Assert.That(result, Is.Zero);
+        }
+
+        [Test]
+        public void WHEN_null_string_passed_THEN_throws_ArgumentNullException()
+        {
+            var sut = new RomanNumbersConverter();
+            Assert.Throws<ArgumentNullException>(() => sut.ToInteger(null));
         }
     }
 }
