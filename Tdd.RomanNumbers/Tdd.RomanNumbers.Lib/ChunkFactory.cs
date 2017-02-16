435e1fa6b0d86103c97fdaca96471847400e1e07
Robert Skarżycki <robert.skarzycki@gmail.com>
2017-02-16 20:48:08 +0100

    Letters and chunk factory introduced

---
+++ Tdd.RomanNumbers/Tdd.RomanNumbers.Lib/ChunkFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tdd.RomanNumbers.Lib
+{
+    class ChunkFactory
+    {
+    }
+
+    internal interface IChunk
+    {
+        ILetter MainLetter { get; }
+        int Value { get; }     
+    }
+
+    internal class SingleLetterChunk : IChunk
+    {
+        public SingleLetterChunk(ILetter letter)
+        {
+            this.MainLetter = letter;
+        }
+
+        public ILetter MainLetter { get; }
+
+        public int Value => this.MainLetter.Value;
+    }
+
+    internal class SubtractionChunk : IChunk
+    {
+        public SubtractionChunk(ILetter minuend, ILetter subtrahend, int repetitionsCount)
+        {
+            if (minuend.AllowedNeighbour != subtrahend)
+            {
+                throw new ArgumentException($"{subtrahend} must not be a subtrahend to {minuend}.");
+            }
+        }
+
+        public int Value { get; }
+
+        public ILetter MainLetter { get; }
+    }
+}
---
+++ Tdd.RomanNumbers/Tdd.RomanNumbers.Lib/Letters.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Tdd.RomanNumbers.Lib
+{
+    internal class Letters
+    {
+        public static ILetter I => new Letter('I', 1);
+        public static ILetter V => new Letter('V', 5, Letters.I);
+        public static ILetter X => new Letter('X', 10, Letters.I);
+        public static ILetter L => new Letter('L', 50, Letters.X);
+        public static ILetter C => new Letter('C', 100, Letters.X);
+        public static ILetter D => new Letter('D', 500, Letters.C);
+        public static ILetter M => new Letter('M', 1000, Letters.C);
+
+        public static IEnumerable<ILetter> Available = new[] {
+            I, V, X, L, C, D, M
+        };
+    }
+
+    internal interface ILetter
+    {
+        char Character { get; }
+        int Value { get; }
+        ILetter AllowedNeighbour { get; }        
+    }
+
+    internal class Letter : ILetter
+    {
+        public Letter(char character, int value, ILetter allowedNeighbour = null)
+        {
+            this.Character = character.ToString().ToUpper().First();
+            this.AllowedNeighbour = allowedNeighbour;
+            this.Value = value;
+        }
+
+        public char Character { get; }
+        public ILetter AllowedNeighbour { get; }
+        public int Value { get; }
+    }    
+}
