50f5b5139e354a4ab5ab0ad10747e45f25ca11e0
Robert Skarżycki <robert.skarzycki@gmail.com>
2017-02-16 21:05:00 +0100

    Letters must be read-only fields, not properties

--- Tdd.RomanNumbers/Tdd.RomanNumbers.Lib/Letters.cs
+++ Tdd.RomanNumbers/Tdd.RomanNumbers.Lib/Letters.cs
@@ -5,13 +5,13 @@ namespace Tdd.RomanNumbers.Lib
 {
     internal class Letters
     {
-        public static ILetter I => new Letter('I', 1);
-        public static ILetter V => new Letter('V', 5, Letters.I);
-        public static ILetter X => new Letter('X', 10, Letters.I);
-        public static ILetter L => new Letter('L', 50, Letters.X);
-        public static ILetter C => new Letter('C', 100, Letters.X);
-        public static ILetter D => new Letter('D', 500, Letters.C);
-        public static ILetter M => new Letter('M', 1000, Letters.C);
+        public static readonly ILetter I = new Letter('I', 1);
+        public static readonly ILetter V = new Letter('V', 5, Letters.I);
+        public static readonly ILetter X = new Letter('X', 10, Letters.I);
+        public static readonly ILetter L = new Letter('L', 50, Letters.X);
+        public static readonly ILetter C = new Letter('C', 100, Letters.X);
+        public static readonly ILetter D = new Letter('D', 500, Letters.C);
+        public static readonly ILetter M = new Letter('M', 1000, Letters.C);
 
         public static IEnumerable<ILetter> Available = new[] {
             I, V, X, L, C, D, M
@@ -37,5 +37,10 @@ namespace Tdd.RomanNumbers.Lib
         public char Character { get; }
         public ILetter AllowedNeighbour { get; }
         public int Value { get; }
+
+        public override string ToString()
+        {
+            return this.Character.ToString();
+        }
     }    
 }
