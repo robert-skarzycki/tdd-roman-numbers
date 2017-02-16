8f4d2c22c653a56e3f492bf72c446e836081644d
Robert Skarżycki <robert.skarzycki@gmail.com>
2017-02-16 20:49:41 +0100

    SingleLetterChunk test

---
+++ Tdd.RomanNumbers/Tdd.RomanNumbers.Lib.Tests/ChunksTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Tdd.RomanNumbers.Lib.Tests
+{
+    [TestFixture]
+    public class ChunksTests
+    {
+        [Test]
+        public void SingleLetterChunk_should_return_value_of_provided_letter()
+        {
+            var letter = Letters.X;
+
+            var sut = new SingleLetterChunk(letter);
+
+            Assert.That(sut.Value, Is.EqualTo(letter.Value));
+        }
+    }
+}
